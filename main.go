7b68269522d0f2ac4579c50727eeaafdda13ef05
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-22 20:40:37 +0530

    Update go.main
    Learnt how to initialize the data type of variables with their data types

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var conference_name = "Go Conference"
-	const conference_tickets = 50
-	var remaining_tickets = 50
+	var conference_name string = "Go Conference"
+	const conference_tickets int16 = 50
+	var remaining_tickets int16 = 50
 
 	fmt.Printf("Welcome to %v booking appilication\n", conference_name)
 	fmt.Printf("We have total of %d titckets and %d are available!\n", conference_tickets, remaining_tickets)
 	fmt.Printf("Get your tickets here to attend\n")
+
+	var user_name string
+	var user_tickets int16
+
+	// User creds
+	user_name = "Tom"
+	user_tickets = 2
+	fmt.Printf("User %s booked %d tickets.\n", user_name, user_tickets)
+
 }
