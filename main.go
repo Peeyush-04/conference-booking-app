09b7eda34cf51c89e43a6a5c4fc9966fb5251d76
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-22 22:51:02 +0530

    Update go.main
    Learnt How to use for loop

--- main.go
+++ main.go
@@ -16,25 +16,35 @@ func main() {
 	var last_name string
 	var email string
 	var user_tickets uint8
+	index := uint8(0)
+	for {
+		// User creds
+		fmt.Printf("\nFirst Name: ")
+		fmt.Scan(&first_name)
+		fmt.Printf("Last Name: ")
+		fmt.Scan(&last_name)
+		fmt.Printf("Email: ")
+		fmt.Scan(&email)
+		fmt.Printf("Booking Tickets: ")
+		fmt.Scan(&user_tickets)
+		fmt.Printf("\n")
 
-	// User creds
-	fmt.Printf("\nFirst Name: ")
-	fmt.Scan(&first_name)
-	fmt.Printf("Last Name: ")
-	fmt.Scan(&last_name)
-	fmt.Printf("Email: ")
-	fmt.Scan(&email)
-	fmt.Printf("Booking Tickets: ")
-	fmt.Scan(&user_tickets)
-	fmt.Printf("\n")
-
-	if user_tickets > remaining_tickets {
-		fmt.Printf("Booking limit exceeded, only %d are currently available!\n", remaining_tickets)
-	} else {
-		fmt.Printf("User %s booked %d tickets.\n\n", first_name, user_tickets)
-		remaining_tickets -= user_tickets
-		bookings = append(bookings, first_name+" "+last_name)
-		fmt.Printf("Remaing Tickets: %d\n\n", remaining_tickets)
-		fmt.Printf("Thank you %s for booking %d tickets.\nYou will recieve the confirmantion email at %s", bookings[0], user_tickets, email)
+		if user_tickets > remaining_tickets {
+			fmt.Printf("Booking limit exceeded, only %d are currently available!\n", remaining_tickets)
+			break
+		} else {
+			fmt.Printf("User %s booked %d tickets.\n\n", first_name, user_tickets)
+			remaining_tickets -= user_tickets
+			bookings = append(bookings, first_name+" "+last_name)
+			fmt.Printf("Remaing Tickets: %d\n\n", remaining_tickets)
+			fmt.Printf("Thank you %s for booking %d tickets.\nYou will recieve the confirmantion email at %s\n", bookings[index], user_tickets, email)
+			if remaining_tickets == 0 {
+				break
+			}
+		}
+		index++
 	}
+
+	// Printing Final Booking List
+	fmt.Println("Final Booking list: ", bookings)
 }
