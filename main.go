6db78cc957c2ceb1684d13fccfe01783285f658a
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-22 20:43:06 +0530

    Update main.go
    Explicitly type casting the variables to save the memory

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 	var conference_name string = "Go Conference"
-	const conference_tickets int16 = 50
-	var remaining_tickets int16 = 50
+	const conference_tickets uint8 = 50
+	var remaining_tickets uint8 = 50
 
 	fmt.Printf("Welcome to %v booking appilication\n", conference_name)
 	fmt.Printf("We have total of %d titckets and %d are available!\n", conference_tickets, remaining_tickets)
 	fmt.Printf("Get your tickets here to attend\n")
 
 	var user_name string
-	var user_tickets int16
+	var user_tickets uint8
 
 	// User creds
 	user_name = "Tom"
