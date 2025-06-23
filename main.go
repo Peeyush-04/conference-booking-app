5559eb99999c872874b345dda2d14d000f6afe6a
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-23 16:21:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,33 +5,6 @@ import (
 	"strings"
 )
 
-func booking_logic(
-	bookings *[]string,
-	index *uint8,
-	first_name *string,
-	last_name *string,
-	user_tickets *uint8,
-	remaining_tickets *uint8,
-	email *string,
-) bool {
-	full_name := *first_name + " " + *last_name
-	if *user_tickets > *remaining_tickets {
-		fmt.Printf("Booking limit exceeded, only %d are currently avaiable!\n", *remaining_tickets)
-		return true
-	} else {
-		fmt.Printf("%s has booked %d tickets.\n\n", *first_name, *user_tickets)
-		*remaining_tickets -= *user_tickets
-		*bookings = append(*bookings, full_name)
-		fmt.Printf("Remaining tickets: %d\n\n", *remaining_tickets)
-		fmt.Printf("Thank you %s for booking %d tickets.\nYou will receive the confirmation email at %s.\n", full_name, *user_tickets, *email)
-		if *remaining_tickets == 0 {
-			return false
-		}
-	}
-	*index += 1
-	return true
-}
-
 func main() {
 	conference_name := string("Go Conference")
 	const conference_tickets uint8 = 50 // conference limit
@@ -60,9 +33,8 @@ func main() {
 		fmt.Scan(&last_name)
 
 		// valid name check
-		valid_name := len(first_name) >= 2 && len(last_name) >= 2
 		for {
-			if !valid_name {
+			if !(len(first_name) >= 2 && len(last_name) >= 2) {
 				fmt.Printf("\n%s %s is not a valid name.\nFirst name and Last name should be greater that equal to 2.\n", first_name, last_name)
 				// input valid name
 				fmt.Printf("\nFirst Name: ")
@@ -79,12 +51,11 @@ func main() {
 		fmt.Scan(&email)
 
 		// valid email check
-		valid_email := strings.Contains(email, "@gmail.com")
 		for {
-			if !valid_email {
-				fmt.Printf("\n%s is not a valid email.\nUse @gmail.com at last.", email)
+			if !(strings.Contains(email, "@gmail.com")) {
+				fmt.Printf("\n%s is not a valid email.\nUse @gmail.com at last.\n", email)
 				// input valid email
-				fmt.Printf("Email: ")
+				fmt.Printf("\nEmail: ")
 				fmt.Scan(&email)
 			} else {
 				break // valid email found => exit
@@ -96,12 +67,11 @@ func main() {
 		fmt.Scan(&user_tickets)
 
 		// valid user tickets check
-		valid_user_tickets := user_tickets > 0 && user_tickets <= remaining_tickets
 		for {
-			if !valid_user_tickets {
+			if !(user_tickets > 0 && user_tickets <= remaining_tickets) {
 				fmt.Printf("\n%d are invalid number of tickets.\nRemaining tickets: %d\nValue should range from 1 to %d", user_tickets, remaining_tickets, remaining_tickets)
 				// input valid user tickets
-				fmt.Printf("\nBooking Tickets: ")
+				fmt.Printf("\n\nBooking Tickets: ")
 				fmt.Scan(&user_tickets)
 			} else {
 				break
