6cd16bd5a4c20ead401c524e22b961c57360b138
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-25 19:31:50 +0530

    Included PasswordHash in User table for auth

--- postgres/init.sql
+++ postgres/init.sql
@@ -4,6 +4,7 @@ create table if not exists users(
     first_name text not null,
     last_name text not null,
     email text not null unique,
+    password_hash text not null,
     role text not null check (role in ('customer', 'organizer')),
     created_at timestamptz not null default now()
 );
