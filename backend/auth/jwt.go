156a92cc9221f26029363967c66ca8d4c9c4c1ec
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-27 17:25:50 +0530

    Included GenerateJWT, ValidateJWT and ExtractJWT for auth purposes

---
+++ backend/auth/jwt.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var jwtSecret = []byte("key")
+
+// generate jwt for signed token for a user
+func GenerateJWT(userID uint32, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+// validats signed jwt
+func ValidateJWT(tokenString string) (*jwt.Token, error) {
+	// parses jwt
+	token, err := jwt.Parse(tokenString,
+		func(t *jwt.Token) (interface{}, error) {
+			// ensure algorithm is HMAC
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("unexpected signing method")
+			}
+			return jwtSecret, nil
+		})
+
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid or expired token")
+	}
+
+	return token, nil
+}
+
+// extracts user id and role from a validated token
+func ExtractClaims(token *jwt.Token) (uint32, string, error) {
+	// get claims
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, "", errors.New("could not parse claims")
+	}
+
+	// safe assertions
+	userIDFloat, ok := claims["user_id"].(float64)
+	if !ok {
+		return 0, "", errors.New("user id not found in token")
+	}
+
+	role, ok := claims["role"].(string)
+	if !ok {
+		return 0, "", errors.New("role not found in token")
+	}
+
+	return uint32(userIDFloat), role, nil
+}
